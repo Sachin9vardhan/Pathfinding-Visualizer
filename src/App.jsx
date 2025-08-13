f92e0b637a78d30f170e1320fd56d475caf2f62b
SACHIN VARDHAN <165420880+Sachin9vardhan@users.noreply.github.com>
2025-08-13 23:16:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
     toggleWall,
     moveStartOrEnd,
     animateAlgorithm,
+    stopAnimation // <-- destructure from hook
   } = usePathfinding();
 
   const [isMousePressed, setIsMousePressed] = useState(false);
